a95c92a7c58e51ffc1b4ae29a2799b631cbafcea
Deaf-Web-Admin <linux.user@techie.com>
2023-07-17 18:45:57 +0200

    Correccion Links de Imagenes/Comprension de Codigo/Proyecto Cerrado

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,9 +3,9 @@ import { Button } from "../Button/Button";
 export const Footer = () => `
 <h2>Contact</h2>
 <div>
-${Button("/icons/twitter.png", "Twitter")}
-${Button("/icons/github.png", "GitHub")}
-${Button("/icons/linkedin.png", "LinkedIn")}
-${Button("/icons/telegram.png", "Telegram")}
+${Button("/Icons/twitter.svg", "Twitter")}
+${Button("/Icons/github.svg", "GitHub")}
+${Button("/Icons/linkedin.svg", "LinkedIn")}
+${Button("/Icons/telegram.svg", "Telegram")}
 </div>
 `;
--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -7,10 +7,10 @@ export const ProjectCard = (project) => `
 <h2>${project.title}</h2>
 <div>
 <a href=${project.github}>
-<img src="/icons/github.png" alt="GitHub Icon" />
+<img src="/Icons/github.svg" alt="GitHub Icon" />
 </a>
 <a href=${project.link}>
-<img src="/icons/link.png" alt="Link icon" />
+<img src="/Icons/link_chain.svg" alt="Link icon" />
 </a>
 </div>
 </div>
